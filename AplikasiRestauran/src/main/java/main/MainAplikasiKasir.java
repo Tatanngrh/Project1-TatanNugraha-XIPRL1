da5810580437d52468e74a4cbc6974c3958bb4b0
T²N <75306063+Tatuns@users.noreply.github.com>
2021-06-16 10:43:29 +0700

    Modul D: Looping Transaksi Lagi

--- AplikasiRestauran/src/main/java/main/MainAplikasiKasir.java
+++ AplikasiRestauran/src/main/java/main/MainAplikasiKasir.java
@@ -32,7 +32,7 @@ public class MainAplikasiKasir {
         app.generateDaftarMenu();
         //Mulai Transaksi
         System.out.println("============ TRANSAKSI ============");
-
+    do {
         //Ambil data Transaksi
         System.out.print("No Transaksi : ");
         no_transaksi = input.next();
@@ -146,10 +146,14 @@ public class MainAplikasiKasir {
                 break;
             }
         } while (kembalian < 0);
+        
+        System.out.println("Lakukan Transaksi Lagi? [Y/])");
+            transaksi_lagi = input.next();
+        } while (transaksi_lagi.equalsIgnoreCase("Y"));
 
         System.out.println("============ TERIMA KASIH ============");
-    }
-    public void generateDaftarMenu() {
+    
+    public void generateDaftaMenu() {
         DaftarMenu daftarMenu = new DaftarMenu();
         //Membuat Menu Ramen
         daftarMenu.tambahMenu(new Ramen("Ramen Seafood", 25000));
