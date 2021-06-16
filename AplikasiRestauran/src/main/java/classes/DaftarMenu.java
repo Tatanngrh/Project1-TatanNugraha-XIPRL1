a07805036f672fe4469bbf5c7bef834c032960dd
T²N <75306063+Tatuns@users.noreply.github.com>
2021-06-16 09:27:55 +0700

    Implementasi Kelas Daftar Menu

--- AplikasiRestauran/src/main/java/classes/DaftarMenu.java
+++ AplikasiRestauran/src/main/java/classes/DaftarMenu.java
@@ -14,8 +14,27 @@ import java.util.ArrayList;
 public class DaftarMenu {
     private ArrayList<Menu> daftarMenu;
 
-    public DaftarMenu() {}
-    public void tambahMenu(Menu menu) {}
-    public void getMenuByKategori(String kategori) {}
-    public void tampilDaftarMenu() {}
+    public DaftarMenu() {
+        daftarMenu = new ArrayList<>();
+    }
+    public void tambahMenu(Menu menu) {
+        daftarMenu.add(menu);
+    }
+    public void getMenuByKategori(String kategori) {
+        System.out.println("======== " + kategori + "========");
+
+        for (int i = 0; 1 <daftarMenu.size(); i++) {
+            Menu m = daftarMenu.get(i);
+            if (m.getKategori().equals(kategori)) {
+                System.out.println((i = 1) + ". " + m.getNama_menu() + "/t" + m.getHarga());
+            }
+        }
+    }
+    public void tampilDaftarMenu() {
+        System.out.println("======== KedaiT2N ========");
+        getMenuByKategori("Ramen");
+        getMenuByKategori("Kuah");
+        getMenuByKategori("Toping");
+        getMenuByKategori("Minuman");
+    }
 }
